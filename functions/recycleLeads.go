f48e49cd2cc347792aab21b87443c2480cc9b4f5
louis <louis.theron@hotmail.com>
2024-07-20 14:04:37 +0200

    Beta

--- functions/recycleLeads.go
+++ functions/recycleLeads.go
@@ -16,44 +16,44 @@ type hopperLead struct {
 	priority       int
 }
 
-func RecycleLeads(campaign_settings models.CampaignSettings, campaign models.Agent_count, DBconn *sql.DB, hopperLevelNeeded int, dncNumbers *models.DNCNumbers, dncNumbersCampaign *models.DNCNumbersCampaign) {
+func RecycleLeads(campaignSettings models.CampaignSettings, campaign models.Agent_count, DBconn *sql.DB, hopperLevelNeeded int, dncNumbers *models.DNCNumbers, dncNumbersCampaign *models.DNCNumbersCampaign) {
 
 	campaignActiveLists := getCampaignActiveLists(DBconn, campaign.Campaign_id)
 	if len(campaignActiveLists) == 0 {
 		println("No active lists")
 		return
 	}
-	var recycle_rules []models.Lead_recycle_rule
+	var recycleRules []models.Lead_recycle_rule
 	fmt.Println("Need to add leads to hopper")
-	recycle_rules = getRecycleRules(DBconn, campaign.Campaign_id)
-	fmt.Println(recycle_rules)
+	recycleRules = getRecycleRules(DBconn, campaign.Campaign_id)
+	fmt.Println(recycleRules)
 
 	var newCount int
 	switch {
-	case strings.HasSuffix(campaign_settings.Lead_order, " 2nd NEW"):
+	case strings.HasSuffix(campaignSettings.Lead_order, " 2nd NEW"):
 		newCount = 2
-	case strings.HasSuffix(campaign_settings.Lead_order, " 3rd NEW"):
+	case strings.HasSuffix(campaignSettings.Lead_order, " 3rd NEW"):
 		newCount = 3
-	case strings.HasSuffix(campaign_settings.Lead_order, " 4th NEW"):
+	case strings.HasSuffix(campaignSettings.Lead_order, " 4th NEW"):
 		newCount = 4
-	case strings.HasSuffix(campaign_settings.Lead_order, " 5th NEW"):
+	case strings.HasSuffix(campaignSettings.Lead_order, " 5th NEW"):
 		newCount = 5
-	case strings.HasSuffix(campaign_settings.Lead_order, " 6th NEW"):
+	case strings.HasSuffix(campaignSettings.Lead_order, " 6th NEW"):
 		newCount = 6
 	}
 
 	FinishedSqlQueryWhere := ""
-	for _, rule := range recycle_rules {
+	for _, rule := range recycleRules {
 		if newCount > 0 && rule.Status == "NEW" {
 			println("New count is greater than 0 and rule status is NEW so skipping")
 			continue
 		}
 		inDialStatus := false
-		for i, status := range campaign_settings.Dial_statuses {
+		for i, status := range campaignSettings.Dial_statuses {
 			if status == rule.Status {
 				inDialStatus = true
 				//remove the dial status from the list
-				campaign_settings.Dial_statuses = append(campaign_settings.Dial_statuses[:i], campaign_settings.Dial_statuses[i+1:]...)
+				campaignSettings.Dial_statuses = append(campaignSettings.Dial_statuses[:i], campaignSettings.Dial_statuses[i+1:]...)
 				break
 			}
 		}
@@ -74,7 +74,7 @@ func RecycleLeads(campaign_settings models.CampaignSettings, campaign models.Age
 		}
 
 	}
-	for _, status := range campaign_settings.Dial_statuses {
+	for _, status := range campaignSettings.Dial_statuses {
 		if newCount > 0 && status == "NEW" {
 			println("New count is greater than 0 and status is NEW so skipping")
 			continue
@@ -86,70 +86,70 @@ func RecycleLeads(campaign_settings models.CampaignSettings, campaign models.Age
 	}
 
 	FinishedSqlQueryWhere = "(" + FinishedSqlQueryWhere + ")"
-	if campaign_settings.Call_count_limit > 0 {
-		FinishedSqlQueryWhere = FinishedSqlQueryWhere + " AND called_count < " + strconv.Itoa(campaign_settings.Call_count_limit)
+	if campaignSettings.Call_count_limit > 0 {
+		FinishedSqlQueryWhere = FinishedSqlQueryWhere + " AND called_count < " + strconv.Itoa(campaignSettings.Call_count_limit)
 	}
 	activeListsString := strings.Join(campaignActiveLists, ",")
 	FinishedSqlQueryWhere = FinishedSqlQueryWhere + " AND list_id IN(" + activeListsString + ")"
 
-	if campaign_settings.Lead_filter_id != "" {
-		FinishedSqlQueryWhere = FinishedSqlQueryWhere + " AND " + campaign_settings.Lead_filter_id + " AND lead_id NOT IN (SELECT lead_id FROM vicidial_hopper)"
+	if campaignSettings.Lead_filter_id != "" {
+		FinishedSqlQueryWhere = FinishedSqlQueryWhere + " AND " + campaignSettings.Lead_filter_id + " AND lead_id NOT IN (SELECT lead_id FROM vicidial_hopper)"
 	}
 
 	var orderStmt string
 	switch {
-	case strings.HasPrefix(campaign_settings.Lead_order, "UP LAST NAME"):
+	case strings.HasPrefix(campaignSettings.Lead_order, "UP LAST NAME"):
 		orderStmt = "order by last_name desc, "
-	case strings.HasPrefix(campaign_settings.Lead_order, "DOWN LAST NAME"):
+	case strings.HasPrefix(campaignSettings.Lead_order, "DOWN LAST NAME"):
 		orderStmt = "order by last_name, "
-	case strings.HasPrefix(campaign_settings.Lead_order, "UP PHONE"):
+	case strings.HasPrefix(campaignSettings.Lead_order, "UP PHONE"):
 		orderStmt = "order by phone_number desc, "
-	case strings.HasPrefix(campaign_settings.Lead_order, "DOWN PHONE"):
+	case strings.HasPrefix(campaignSettings.Lead_order, "DOWN PHONE"):
 		orderStmt = "order by phone_number, "
-	case strings.HasPrefix(campaign_settings.Lead_order, "UP COUNT"):
+	case strings.HasPrefix(campaignSettings.Lead_order, "UP COUNT"):
 		orderStmt = "order by called_count desc, "
-	case strings.HasPrefix(campaign_settings.Lead_order, "DOWN COUNT"):
+	case strings.HasPrefix(campaignSettings.Lead_order, "DOWN COUNT"):
 		orderStmt = "order by called_count, "
-	case strings.HasPrefix(campaign_settings.Lead_order, "UP LAST CALL TIME"):
+	case strings.HasPrefix(campaignSettings.Lead_order, "UP LAST CALL TIME"):
 		orderStmt = "order by last_local_call_time desc, "
-	case strings.HasPrefix(campaign_settings.Lead_order, "DOWN LAST CALL TIME"):
+	case strings.HasPrefix(campaignSettings.Lead_order, "DOWN LAST CALL TIME"):
 		orderStmt = "order by last_local_call_time, "
-	case strings.HasPrefix(campaign_settings.Lead_order, "UP RANK"):
+	case strings.HasPrefix(campaignSettings.Lead_order, "UP RANK"):
 		orderStmt = "order by rank desc, "
-	case strings.HasPrefix(campaign_settings.Lead_order, "DOWN RANK"):
+	case strings.HasPrefix(campaignSettings.Lead_order, "DOWN RANK"):
 		orderStmt = "order by rank, "
-	case strings.HasPrefix(campaign_settings.Lead_order, "UP OWNER"):
+	case strings.HasPrefix(campaignSettings.Lead_order, "UP OWNER"):
 		orderStmt = "order by owner desc, "
-	case strings.HasPrefix(campaign_settings.Lead_order, "DOWN OWNER"):
+	case strings.HasPrefix(campaignSettings.Lead_order, "DOWN OWNER"):
 		orderStmt = "order by owner, "
-	case strings.HasPrefix(campaign_settings.Lead_order, "UP TIMEZONE"):
+	case strings.HasPrefix(campaignSettings.Lead_order, "UP TIMEZONE"):
 		orderStmt = "order by gmt_offset_now desc, "
-	case strings.HasPrefix(campaign_settings.Lead_order, "DOWN TIMEZONE"):
+	case strings.HasPrefix(campaignSettings.Lead_order, "DOWN TIMEZONE"):
 		orderStmt = "order by gmt_offset_now, "
-	case strings.HasPrefix(campaign_settings.Lead_order, "RANDOM"):
+	case strings.HasPrefix(campaignSettings.Lead_order, "RANDOM"):
 		orderStmt = "order by RAND(), "
-	case strings.HasPrefix(campaign_settings.Lead_order, "UP"):
+	case strings.HasPrefix(campaignSettings.Lead_order, "UP"):
 		orderStmt = "order by lead_id desc, "
-	case strings.HasPrefix(campaign_settings.Lead_order, "DOWN"):
+	case strings.HasPrefix(campaignSettings.Lead_order, "DOWN"):
 		orderStmt = "order by lead_id asc, "
 	}
 
 	println(newCount)
-	var secondary_order_stmt string
-	switch campaign_settings.Lead_order_secondary {
+	var secondaryOrderStmt string
+	switch campaignSettings.Lead_order_secondary {
 	case "LEAD_ASCEND":
-		secondary_order_stmt = "lead_id asc"
+		secondaryOrderStmt = "lead_id asc"
 	case "LEAD_DESCEND":
-		secondary_order_stmt = "lead_id desc"
+		secondaryOrderStmt = "lead_id desc"
 	case "CALLTIME_ASCEND":
-		secondary_order_stmt = "last_local_call_time asc"
+		secondaryOrderStmt = "last_local_call_time asc"
 	case "CALLTIME_DESCEND":
-		secondary_order_stmt = "last_local_call_time desc"
+		secondaryOrderStmt = "last_local_call_time desc"
 	default:
-		secondary_order_stmt = ""
+		secondaryOrderStmt = ""
 	}
 
-	FinishedSqlQueryWhere = FinishedSqlQueryWhere + " " + orderStmt + secondary_order_stmt
+	FinishedSqlQueryWhere = FinishedSqlQueryWhere + " " + orderStmt + secondaryOrderStmt
 
 	// Wait for DNC numbers to be loaded before proceeding
 	dncNumbers.Mu.RLock()
@@ -163,27 +163,32 @@ func RecycleLeads(campaign_settings models.CampaignSettings, campaign models.Age
 		println(err)
 		return
 	}
-	defer rows.Close()
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			println(err.Error())
+		}
+	}(rows)
 	var recycleHopperLeads []hopperLead
 	for rows.Next() {
-		var lead_id string
-		var phone_number string
-		var list_id string
-		var vendor_lead_code string
+		var leadId string
+		var phoneNumber string
+		var listId string
+		var vendorLeadCode string
 		var doNotLoadLead bool
 		var dncStatus string
-		err := rows.Scan(&lead_id, &phone_number, &list_id, &vendor_lead_code)
+		err := rows.Scan(&leadId, &phoneNumber, &listId, &vendorLeadCode)
 		if err != nil {
 			println(err)
 			return
 		}
 		// Check if the number is in DNC
-		if dncNumbers.Numbers[phone_number] {
+		if dncNumbers.Numbers[phoneNumber] {
 			println("Number is in DNC")
 			dncStatus = "DNCL"
 			doNotLoadLead = true
 		}
-		if dncNumbersCampaign.Numbers[phone_number] {
+		if dncNumbersCampaign.Numbers[phoneNumber] {
 			println("Number is in campaign DNC")
 			dncStatus = "DNCC"
 			doNotLoadLead = true
@@ -191,34 +196,39 @@ func RecycleLeads(campaign_settings models.CampaignSettings, campaign models.Age
 		// Add lead to hopper
 		if doNotLoadLead {
 			query := "update vicidial_list set status = ? where lead_id = ?"
-			_, err := DBconn.Exec(query, dncStatus, lead_id)
+			_, err := DBconn.Exec(query, dncStatus, leadId)
 			if err != nil {
 				println(err)
 			}
 		} else {
-			recycleHopperLeads = append(recycleHopperLeads, hopperLead{leadId: lead_id, phoneNumber: phone_number, listId: list_id, vendorLeadCode: vendor_lead_code, priority: 0})
+			recycleHopperLeads = append(recycleHopperLeads, hopperLead{leadId: leadId, phoneNumber: phoneNumber, listId: listId, vendorLeadCode: vendorLeadCode, priority: 0})
 		}
 	}
 	var newLeadHopper []hopperLead
 	if newCount > 0 {
-		query = "SELECT lead_id,phone_number,list_id,vendor_lead_code FROM vicidial_list WHERE status = 'NEW' AND list_id IN(" + activeListsString + ") AND lead_id NOT IN (SELECT lead_id FROM vicidial_hopper)" + " " + orderStmt + secondary_order_stmt + " LIMIT " + strconv.Itoa(hopperLevelNeeded)
+		query = "SELECT lead_id,phone_number,list_id,vendor_lead_code FROM vicidial_list WHERE status = 'NEW' AND list_id IN(" + activeListsString + ") AND lead_id NOT IN (SELECT lead_id FROM vicidial_hopper)" + " " + orderStmt + secondaryOrderStmt + " LIMIT " + strconv.Itoa(hopperLevelNeeded)
 		rows, err := DBconn.Query(query)
 		if err != nil {
 			println(err)
 			return
 		}
-		defer rows.Close()
+		defer func(rows *sql.Rows) {
+			err := rows.Close()
+			if err != nil {
+				println(err.Error())
+			}
+		}(rows)
 		for rows.Next() {
-			var lead_id string
-			var phone_number string
-			var list_id string
-			var vendor_lead_code string
-			err := rows.Scan(&lead_id, &phone_number, &list_id, &vendor_lead_code)
+			var leadId string
+			var phoneNumber string
+			var listId string
+			var vendorLeadCode string
+			err := rows.Scan(&leadId, &phoneNumber, &listId, &vendorLeadCode)
 			if err != nil {
 				println(err)
 				return
 			}
-			newLeadHopper = append(newLeadHopper, hopperLead{leadId: lead_id, phoneNumber: phone_number, listId: list_id, vendorLeadCode: vendor_lead_code, priority: 0})
+			newLeadHopper = append(newLeadHopper, hopperLead{leadId: leadId, phoneNumber: phoneNumber, listId: listId, vendorLeadCode: vendorLeadCode, priority: 0})
 		}
 	}
 
@@ -230,19 +240,24 @@ func RecycleLeads(campaign_settings models.CampaignSettings, campaign models.Age
 	if err != nil {
 		println(err)
 	}
-	defer rows.Close()
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			println(err.Error())
+		}
+	}(rows)
 	for rows.Next() {
-		var lead_id string
-		var phone_number string
-		var list_id string
-		var vendor_lead_code string
-		err := rows.Scan(&lead_id, &phone_number, &list_id, &vendor_lead_code)
+		var leadId string
+		var phoneNumber string
+		var listId string
+		var vendorLeadCode string
+		err := rows.Scan(&leadId, &phoneNumber, &listId, &vendorLeadCode)
 		if err != nil {
 			println(err)
 			return
 		}
-		fmt.Printf("Adding Callback lead %s to hopper\n", lead_id)
-		hopperLeads = append(hopperLeads, hopperLead{leadId: lead_id, phoneNumber: phone_number, listId: list_id, vendorLeadCode: vendor_lead_code, priority: 50})
+		fmt.Printf("Adding Callback lead %s to hopper\n", leadId)
+		hopperLeads = append(hopperLeads, hopperLead{leadId: leadId, phoneNumber: phoneNumber, listId: listId, vendorLeadCode: vendorLeadCode, priority: 50})
 	}
 
 	var recycleLeadCounter = 1
